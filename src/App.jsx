47cf6c13653b5c9dce89296318376f3a44a00ee1
MatDV <matdv@matdvs-macbook-pro.home>
2024-08-19 13:54:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Lorem</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+          quis repellat alias libero aperiam omnis suscipit consequuntur
+          similique animi. Accusamus perspiciatis ex facere aliquid iste,
+          cupiditate harum. Voluptate, hic libero? Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Suscipit, autem. Earum assumenda at
+          eaque tenetur debitis dolorem delectus voluptatum error adipisci saepe
+          ratione velit libero sed facilis, placeat eos laudantium! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Consequatur numquam ullam
+          mollitia impedit cupiditate voluptates odit et provident?
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
